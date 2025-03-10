aa57d89d91c3fa4294039c0939050eec43063747
Maxim Kononov <152982801+0xMe1ok@users.noreply.github.com>
2025-03-10 20:35:00 +0700

    Added string enum convertation

--- Kanvas.Presentation/DependencyInjection.cs
+++ Kanvas.Presentation/DependencyInjection.cs
@@ -1,5 +1,6 @@
-using Infrastructure;
-using Infrastructure.Mapper;
+using System.Text.Json.Serialization;
+using Presentation;
+using Presentation.Mapper;
 using Microsoft.EntityFrameworkCore;
 
 namespace Presentation;
@@ -8,6 +9,12 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddAppDI(this IServiceCollection services)
     {
+        services.AddControllers()
+            .AddJsonOptions(options =>
+            {
+                options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+            });
+        
         services.AddDbContext<ApplicationDbContext>(options => {
                 options.UseNpgsql("Server=127.0.0.1;Port=5432;Database=kanvasdb;Username=pguser;Password=secretpassword;");
             }
