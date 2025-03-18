fff94cd064619fa12b97df8ae544235edd326384
Maxim Kononov <152982801+0xMe1ok@users.noreply.github.com>
2025-03-18 14:10:41 +0700

    Added helpers in Task Repository

--- Kanvas.Presentation/Interfaces/IAppTaskRepository.cs
+++ Kanvas.Presentation/Interfaces/IAppTaskRepository.cs
@@ -4,6 +4,14 @@ namespace Presentation.Interfaces;
 
 public interface IAppTaskRepository : IRepository<AppTask>
 {
-    Task<int> ClearColumnIdInColumn(Guid columnId);
-    Task<int> ClearBoardIdInBoard(Guid boardId);
+    Task<int> ClearColumnIdInColumnAsync(Guid columnId);
+    Task<int> ClearBoardIdInBoardAsync(Guid boardId);
+    
+    Task<int> GetMaxOrderInColumnAsync(Guid columnId);
+
+    Task ShiftTasksOrderAsync(
+        Guid columnId,
+        int startOrder,
+        int endOrder,
+        int shiftBy);
 }
--- Kanvas.Presentation/Repositories/AppTaskRepository.cs
+++ Kanvas.Presentation/Repositories/AppTaskRepository.cs
@@ -10,18 +10,41 @@ public class AppTaskRepository : Repository<AppTask>, IAppTaskRepository
     {
     }
 
-    public async Task<int> ClearColumnIdInColumn(Guid columnId)
+    public async Task<int> ClearColumnIdInColumnAsync(Guid columnId)
     {
         return await _context.AppTasks.Where(task => task.ColumnId == columnId)
              .ExecuteUpdateAsync(setters => setters
                  .SetProperty(t => t.ColumnId, t => null));
     }
 
-    public async Task<int> ClearBoardIdInBoard(Guid boardId)
+    public async Task<int> ClearBoardIdInBoardAsync(Guid boardId)
     {
         return await _context.AppTasks.Where(task => task.BoardId == boardId)
             .ExecuteUpdateAsync(setters => setters
                 .SetProperty(t => t.ColumnId, t=> null)
                 .SetProperty(t => t.BoardId, t => null));
+    }
+
+    public async Task<int> GetMaxOrderInColumnAsync(Guid columnId)
+    {
+        return await _context.AppTasks
+            .Where(t => t.ColumnId == columnId)
+            .MaxAsync(t => (int?)t.Order) ?? 0;
+    }
+    
+    public async Task ShiftTasksOrderAsync(
+        Guid columnId,
+        int startOrder,
+        int endOrder,
+        int shiftBy)
+    {
+        await _context.AppTasks
+            .Where(t => t.ColumnId == columnId)
+            .Where(t => t.Order >= startOrder)
+            .Where(t => t.Order <= endOrder)
+            .ExecuteUpdateAsync(setters => 
+                setters.SetProperty(
+                    t => t.Order,
+                    t => t.Order + shiftBy));
     }
 }
