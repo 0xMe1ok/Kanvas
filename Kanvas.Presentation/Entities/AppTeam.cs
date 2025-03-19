ddc3f80c9eac23294d5a8950b01168db71612a5c
Maxim Kononov <152982801+0xMe1ok@users.noreply.github.com>
2025-03-19 23:24:08 +0700

    Added Members in teams

--- Kanvas.Presentation/Entities/AppTeam.cs
+++ Kanvas.Presentation/Entities/AppTeam.cs
@@ -7,4 +7,5 @@ public class AppTeam : EntityBase<Guid>
     public Guid OwnerId { get; set; }
     public List<TaskBoard> Boards { get; set; }
     public List<AppTask> Tasks { get; set; }
+    public List<TeamMember> Members { get; set; }
 }
