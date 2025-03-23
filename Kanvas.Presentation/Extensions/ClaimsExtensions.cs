0e5cf66e5dca499b65c59345d1c8a4621bedd350
Maxim Kononov <152982801+0xMe1ok@users.noreply.github.com>
2025-03-23 20:29:45 +0700

    fix: Explicit statement for ClaimsTypes in ClaimsExtensions.cs

--- Kanvas.Presentation/Extensions/ClaimsExtensions.cs
+++ Kanvas.Presentation/Extensions/ClaimsExtensions.cs
@@ -5,6 +5,6 @@ namespace Presentation.Extensions;
 public static class ClaimsExtensions
 {
     public static string GetUsername(this ClaimsPrincipal user) {
-        return user.Claims.SingleOrDefault(x => x.Type.Equals("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/givenname")).Value;
+        return user.Claims.SingleOrDefault(x => x.Type.Equals(ClaimTypes.GivenName)).Value;
     }
 }
