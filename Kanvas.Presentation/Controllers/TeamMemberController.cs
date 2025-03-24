a292a76dcf4e169212225635c8848a9aa1d7dd61
Maxim Kononov <152982801+0xMe1ok@users.noreply.github.com>
2025-03-24 22:19:50 +0700

    fix: Add role check to team member controller

--- Kanvas.Presentation/Controllers/TeamMemberController.cs
+++ Kanvas.Presentation/Controllers/TeamMemberController.cs
@@ -1,6 +1,8 @@
 using Asp.Versioning;
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using Presentation.Enums;
+using Presentation.Exceptions;
 using Presentation.Interfaces;
 
 namespace Presentation.Controllers;
@@ -12,30 +14,47 @@ public class TeamMemberController : ControllerBase
 {
     private readonly IUnitOfWork _unitOfWork;
     private readonly IMapper _mapper;
+    private readonly IUserContext _userContext;
+    private readonly ITeamRoleService _teamRoleService;
+
 
     public TeamMemberController(
-        IAppTeamService appTeamService,
         IUnitOfWork unitOfWork, 
-        IMapper mapper)
+        IMapper mapper,
+        IUserContext userContext,
+        ITeamRoleService teamRoleService)
     {
         _unitOfWork = unitOfWork;
         _mapper = mapper;
+        _userContext = userContext;
+        _teamRoleService = teamRoleService;
     }
 
     [HttpGet]
     public async Task<IActionResult> GetTeamMembers(Guid teamId)
     {
-        // TODO: Move to service
+        // TODO : move logic to service
+        var userId = _userContext.UserId;
+        if (!await _unitOfWork.TeamMembers.ExistsAsync(teamId, userId))
+            throw new ForbiddenException($"User {userId} does not belong to team");
+        
         var members = await _unitOfWork.TeamMembers.GetAll(teamId);
         return Ok(members);
     }
 
     [HttpDelete]
     [Route("{userId}")]
-    public async Task<IActionResult> DeleteTeamMember(Guid teamId, Guid userId)
+    public async Task<IActionResult> DeleteTeamMember(Guid teamId, Guid userIdToDelete)
     {
-        // TODO: Move to service
-        await _unitOfWork.TeamMembers.RemoveAsync(teamId, userId);
+        // TODO : move logic to service
+        var userId = _userContext.UserId;
+        if (!await _unitOfWork.TeamMembers.ExistsAsync(teamId, userId))
+            throw new ForbiddenException($"User {userId} does not belong to team");
+        
+        if (!await _teamRoleService.IsInTeamRoleOrHigherAsync(userId, teamId, TeamRole.Admin))
+            throw new ForbiddenException($"User does not have permission to delete user in team {teamId}");
+        
+        await _unitOfWork.TeamMembers.RemoveAsync(teamId, userIdToDelete);
         return NoContent();
     }
     
